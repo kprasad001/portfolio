3808ffd034825dc81b21c0eb066862a126deffb3
Kishan <prasadkishan2@outlook.com>
2026-03-02 15:12:04 -0700

    Added footer and projects

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,7 +3,10 @@ import Home from './components/home.jsx'
 import AboutMe from './components/about_me.jsx'
 import Projects from './components/projects.jsx'
 import Experience from './components/experience.jsx'
+import Contact from './components/contact.jsx'
 import { Routes, Route, Link } from 'react-router-dom'
+import { Github, Linkedin, Mail } from 'lucide-react';
+
 
 function App() {
 	return (
@@ -15,7 +18,7 @@ function App() {
 				<Link to="/projects">Projects</Link>
 				<Link to='/experience'>Experience</Link>
 				<Link to="/about">About</Link>
-				<a>Contact</a>
+				<Link to="/contact">Contact</Link>
 			</div>
 		</nav>
 		<main>
@@ -24,8 +27,25 @@ function App() {
 				<Route path="/projects" element={<Projects />} />
 				<Route path="/experience" element={<Experience />} />
 				<Route path="/about" element={<AboutMe />} />
+				<Route path="/contact" element={<Contact />} />
 			</Routes>
 		</main>
+		<footer>
+			<h3>Full Stack Developer</h3>
+			<p>Kishan Prasad</p>
+			<div className='line2'></div>
+			<div className="social-media">
+				<a href="https://github.com/kprasad001" className="github">
+					<Github/>
+				</a>
+				<a href="https://www.linkedin.com/in/kkprasad1/" className="linked-in-icon">
+					<Linkedin/>
+				</a>
+				<a href="mailto:kkprasad@ualberta.ca" className="mail-icon">
+					<Mail/>
+				</a>
+			</div>
+		</footer>
 		</>
 	)
 }
---
+++ portfolio/src/components/contact.jsx
@@ -0,0 +1,14 @@
+import './CSS/home.css'
+import './CSS/contact.css'
+
+function Contact() {
+
+	return (
+		<section className='contact'>
+            <h1>Get In Touch</h1>
+            <div className='line'></div>
+		</section>
+	)
+}
+
+export default Contact
